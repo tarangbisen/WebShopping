02930b0e6c63989b524eee5100d45102719325a2
tarangbisen <105142861+tarangbisen@users.noreply.github.com>
2022-05-19 18:01:59 +0530

    Update dbManager.cs

--- webshopping01/dbManager.cs
+++ webshopping01/dbManager.cs
@@ -16,13 +16,13 @@ namespace webshopping01
 
 {
 
-new Users(){id=0, name="John Doe", email_id="Tarang123@gmail.com",password="12345",address="Kolkata", pin_code = 424550},
+new Users(){id=0, name="Tarang Bisen", email_id="Tarang123@gmail.com",password="12345",address="Kolkata", pin_code = 424550},
 
 new Users(){id=1, name="Atif Hussain", email_id="Atif123@gmail.com",password="54321",address="Patna",pin_code = 429551},
 
 new Users(){id=2, name="Nawaz", email_id="Nawaz123@gmail.com",password="1234",address="Mumbai",pin_code = 424952},
 
-new Users(){id=3, name="Aryan", email_id="Bisen123@gmail.com",password="123",address="Hyderabad",pin_code = 42485},
+new Users(){id=3, name="Bisen", email_id="Bisen123@gmail.com",password="123",address="Hyderabad",pin_code = 42485},
 
 new Users(){id=4, name="Anshuman", email_id="Anshuman123@gmail.com",password="qwert",address="Chennai",pin_code = 42855},
 
